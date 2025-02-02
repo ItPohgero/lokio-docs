b3e4c81c2dd7a64eb3a1e0f3296ed8560a1a4d48
wahyu agus arifin <itpohgero@gmail.com>
2025-02-02 14:01:46 +0700

    up

---
+++ src/app/(install)/sh/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    try {
+        const response = await fetch('http://103.127.96.116:9000/install.sh');
+        const data = await response.text();
+        return new NextResponse(data, { status: 200 });
+    } catch (error) {
+        return new NextResponse(JSON.stringify({ error: 'Failed to fetch data' }), { status: 500 });
+    }
+}
